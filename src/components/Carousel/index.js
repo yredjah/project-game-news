13ededce92f4258b781258a2e74b334c591423c5
GuillaumeBordeau <guillaume.bordeau@outlook.fr>
2019-07-22 13:01:08 +0200

    add simple carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,5 +1,7 @@
 // == Import : npm
 import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel as ReactCarousel } from 'react-responsive-carousel';
 
 // == Import : local
 
@@ -8,7 +10,20 @@ import './carousel.scss';
 // == Composant
 const Carousel = () => (
   <div id="Carousel">
-  Carousel
+    <ReactCarousel infiniteLoop autoPlay>
+      <div>
+      <iframe width="1000" height="1000" src="https://www.youtube.com/embed/5oc_HnczBIU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <p className="legend">Legend 1</p>
+      </div>
+      <div>
+        <img src="src/Assets/img/logo.jpg" />
+        <p className="legend">Legend 2</p>
+      </div>
+      <div>
+        <img src="src/Assets/img/logo.jpg" />
+        <p className="legend">Legend 3</p>
+      </div>
+    </ReactCarousel>
   </div>
 );
 
