4b9913d32f076d32735a0d6c2620209b03004e24
yredjah <y.redjah@gmail.com>
2019-07-23 16:46:31 +0200

    article

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,6 +5,7 @@ import React from 'react';
 import Header from 'src/components/Header';
 import Carousel from 'src/components/Carousel';
 import Articles from 'src/components/Articles';
+import Article from 'src/components/Article';
 import Footer from 'src/components/Footer';
 import './app.scss';
 
