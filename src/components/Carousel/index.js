9d5e83627bfeb06918c31a21d8979ff7f71c9b6e
yredjah <y.redjah@gmail.com>
2019-07-23 13:12:36 +0200

    api youtube on carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,28 +1,61 @@
 // == Import : npm
-import React from 'react';
+import React, { Component } from 'react';
 import Coverflow from 'react-coverflow';
+import YouTube from 'react-youtube';
 
 // == Import : local
 import './carousel.scss';
 
 // == Composant
-const Carousel = () => (
-  <div id="Carousel">
-    <Coverflow
-      width="960"
-      height="500"
-      displayQuantityOfSide={2}
-      navigation={true}
-      enableScroll
-      clickable
-      active={0}
-    >
-      <iframe title="wow" width="560" height="315" src="https://www.youtube.com/embed/TcZyiYOzsSw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      <iframe title="cyber" width="560" height="315" src="https://www.youtube.com/embed/8X2kIfS6fb8" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/Gu8X7vM3Avw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope;picture-in-picture" allowFullScreen></iframe>
-    </Coverflow>
-  </div>
-);
+class Carousel extends Component {
+  state ={}
+  render() {
+
+    const opts = {
+      height: '300',
+      width: '600',
+      playerVars: {
+        controls: 0,
+        modestBranding: 1,
+        showInfo: 0,
+      }
+    };
+
+    return (
+      <div id="Carousel">
+        <Coverflow
+          width="960"
+          height="500"
+          displayQuantityOfSide={2}
+          navigation={true}
+          enableScroll
+          clickable
+          active={0}
+        >
+
+          <YouTube
+            videoId="TcZyiYOzsSw"
+            opts={opts}
+          />
+
+          <YouTube
+            videoId="8X2kIfS6fb8"
+            opts={opts}
+          />
+
+          <YouTube
+            videoId=" Gu8X7vM3Avw"
+            opts={opts}
+          />
+
+        </Coverflow>
+
+      </div>
+
+    );
+  }
+}
+
 
 // == Export
 export default Carousel;
