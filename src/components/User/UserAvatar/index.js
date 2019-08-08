559686893c5ac1f92bdbc800f4bd95c71c24bb79
yredjah <y.redjah@gmail.com>
2019-08-08 11:16:15 +0200

    avatar css

--- src/components/User/UserAvatar/index.js
+++ src/components/User/UserAvatar/index.js
@@ -18,13 +18,8 @@ class UserAvatar extends Component {
   handleUploadImage(ev) {
     ev.preventDefault();
 
-    // console.log('files', ev.target.files[0]);
-
     const data = new FormData();
     data.append('file', this.uploadInput.files[0]);
-    //data.append('filename', this.fileName.value);
-
-    // console.log({ data });
 
     axios.post('http://localhost:3000/api/upload', data)
       .then((response) => {
@@ -38,12 +33,11 @@ class UserAvatar extends Component {
         <div>
           <input ref={(ref) => { this.uploadInput = ref; }} type="file" accept=".jpg, .jpeg, .png, .gif" />
         </div>
-       
         <br />
         <div>
           <button>Upload</button>
         </div>
-        <img src={this.state.imageURL} alt="img" />
+        <img className="avatar-img" src={this.state.imageURL} alt="img" />
       </form>
     );
   }
