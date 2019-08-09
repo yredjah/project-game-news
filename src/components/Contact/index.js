52b41f7bce35062dd0bed12a778d2df33327cb54
yredjah <y.redjah@gmail.com>
2019-08-09 13:03:53 +0200

    logo debug

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -65,7 +65,7 @@ const Contact = ({
       name='contactMessage'
       />
 
-      <Button className="contactButton" type="submit">Submit</Button>
+      <Button type="submit">Submit</Button>
       </Form>
   </div>
 );
